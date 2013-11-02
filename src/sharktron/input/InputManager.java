736fe4b7b830318ce4838d213e86c0c07e2c46de
acrynx <acrynx@acrynx-PC>
2013-11-02 04:26:10 +0100

    Extended InputManager to support Esc, Space and Return keys

--- src/sharktron/input/InputManager.java
+++ src/sharktron/input/InputManager.java
@@ -1,5 +1,6 @@
 package sharktron.input;
 
+import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.geom.Point;
 import sharktron.RuntimeConfiguration;
@@ -36,8 +37,7 @@ public class InputManager
     }
     
     /**
-     * Determines, if the left mouse button is pressed.
-     * Returns boolean accordingly.
+     * Determines, if the left mouse button is down.
      * 
      * @return Left mouse button pressed
      */
@@ -47,13 +47,102 @@ public class InputManager
     }
     
     /**
-     * Determines, if the right mouse button is pressed.
-     * Returns boolean accordingly.
+     * Determines, if the left mouse button is pressed.
      * 
-     * @return Right mouse button pressed
+     * @return Left mouse button pressed
+     */
+    public static boolean isLeftMousePressed()
+    {
+         return input.isMousePressed(0);
+    }
+    
+    /**
+     * Determines, if the right mouse button is down.
+     * 
+     * @return Right mouse button down
      */
     public static boolean isRightMouseDown()
     {
          return input.isMouseButtonDown(1);
+    }
+    
+    /**
+     * Determines, if the right mouse button is pressed.
+     * 
+     * @return Right mouse button pressed
+     */
+    public static boolean isRightMousePressed()
+    {
+         return input.isMousePressed(1);
+    }
+    
+    /**
+     * Determines, if the Escape key is down.
+     * 
+     * @return Escape key down
+     */
+    public static boolean isEscKeyDown()
+    {
+        return input.isKeyDown(Keyboard.KEY_ESCAPE);
+    }
+    
+    /**
+     * Determines, if the Escape key is pressed.
+     * 
+     * @return Escape key pressed
+     */
+    public static boolean isEscKeyPressed()
+    {
+        return input.isKeyPressed(Keyboard.KEY_ESCAPE);
+    }
+    
+    /**
+     * Determines, if the Space key is down.
+     * 
+     * @return Space key down
+     */
+    public static boolean isSpaceKeyDown()
+    {
+        return input.isKeyDown(Keyboard.KEY_SPACE);
+    }
+    
+    /**
+     * Determines, if the Space key is pressed.
+     * 
+     * @return Space key pressed
+     */
+    public static boolean isSpaceKeyPressed()
+    {
+        return input.isKeyPressed(Keyboard.KEY_SPACE);
+    }
+    
+    /**
+     * Determines, if the Return key is down.
+     * 
+     * @return Return key down
+     */
+    public static boolean isReturnKeyDown()
+    {
+        return input.isKeyDown(Keyboard.KEY_RETURN);
+    }
+    
+    /**
+     * Determines, if the Return key is pressed.
+     * 
+     * @return Return key pressed
+     */
+    public static boolean isReturnKeyPressed()
+    {
+        return input.isKeyPressed(Keyboard.KEY_RETURN);
+    }
+    
+    /**
+     * Passes the instance of the Slick2D Input object to the outside world.
+     * 
+     * @return Instance of Slick2D Input
+     */
+    public static Input getInput()
+    {
+        return input;
     }
 }
